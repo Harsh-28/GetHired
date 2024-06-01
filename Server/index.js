5027f7e8f1b1c69daf9ea9803d794d7ae925027d
Harsh-28 <harshsree677@gmail.com>
2024-06-01 14:55:04 +0530

    update index.js

--- Server/index.js
+++ Server/index.js
@@ -5,18 +5,21 @@ const userRoutes = require("./routes/User");
 const profileRoutes = require("./routes/Profile");
 const contactUsRoute = require("./routes/Contact");
 
+const fetchData = require("./fetchData")
 const database = require("./config/dataBase");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const {cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const fetchJobs = require("./fetchJobs");
 
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 //database connect
 database.connect();
+// fetchData();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
@@ -42,6 +45,25 @@ cloudinaryConnect();
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/reach", contactUsRoute);
+app.get('/api/v1/fetch-data', async (req, res) => {
+    try {
+        await fetchData();
+        res.status(200).json({ message: 'Data fetched and written to file successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to fetch data' });
+    }
+});
+
+app.get('/api/v1/jobdata', async (req, res) => {
+    try {
+        await fetchJobs();
+        res.status(200).json({ message: 'Data fetched and written to file successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to fetch data' });
+    }
+});
 
 app.use(express.static('client/build'))
 
